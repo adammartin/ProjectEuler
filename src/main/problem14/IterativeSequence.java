53b04d95b9832aa3493f019050e2fa2018aee9dd
adammartin <ebondark.od@gmail.com>
2011-06-04 09:02:36 -0500

    Refactor of Iterative Sequence

--- src/main/problem14/IterativeSequence.java
+++ src/main/problem14/IterativeSequence.java
@@ -2,6 +2,7 @@ package problem14;
 
 import static java.math.BigInteger.ONE;
 import static java.math.BigInteger.ZERO;
+import static java.math.BigInteger.valueOf;
 
 import java.math.BigInteger;
 
@@ -20,28 +21,35 @@ public class IterativeSequence {
 	}
 
 	public int chainFor(final int start) {
-		int count = 1;
-		BigInteger value = BigInteger.valueOf(start);
-		while(value.compareTo(ONE) > 0) {
-			count++;
-			value = reduce(value);
-		}
-		return count;
+		return chainCount(valueOf(start));
 	}
 
-	private BigInteger reduce(final BigInteger start) {
-		if(start.mod(TWO).compareTo(ZERO) == 0){
-			return start.divide(TWO);
-		}
-		return start.multiply(THREE).add(ONE);
+	private int chainCount(final BigInteger start) {
+		return start.equals(ONE) ? 1 : chainCount(reduce(start)) + 1;
 	}
 	
+	private BigInteger reduce(final BigInteger value) {
+		return isEven(value) ? reduceEven(value) : reduceOdd(value);
+	}
+
+	private boolean isEven(final BigInteger value) {
+		return value.mod(TWO).compareTo(ZERO) == 0;
+	}
+	
+	private BigInteger reduceEven(final BigInteger value) {
+		return value.divide(TWO);
+	}
+	
+	private BigInteger reduceOdd(final BigInteger value) {
+		return value.multiply(THREE).add(ONE);
+	}
+
 	private int countStart(int max) {
-		if(max > 3){
-			int countStart = max/3*2;
-			return countStart % 2 == 0 ? countStart + 1 : countStart;
-		}
-		return max;
+		return max > 3 ? oddValue(max/3*2) : max;
+	}
+
+	private int oddValue(int value) {
+		return value % 2 == 0 ? value + 1 : value;
 	}
 	
 	private class Tuple {
