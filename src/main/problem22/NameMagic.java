c79775e1f8a35d249a913be4946f6f9dcb0a2055
adammartin <ebondark.od@gmail.com>
2011-06-08 01:22:35 -0400

    Problem 22 solved

---
+++ src/main/problem22/NameMagic.java
@@ -0,0 +1,60 @@
+package problem22;
+
+import static java.math.BigInteger.ZERO;
+import static java.math.BigInteger.valueOf;
+import static java.util.Collections.sort;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class NameMagic {
+
+	private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+	private final List<String> names;
+	private final List<Character> alpha = characterArray(ALPHABET);
+
+	public NameMagic(File file) throws FileNotFoundException {
+		names = loadNames(file);
+	}
+
+	public int sum() {
+		BigInteger sum = ZERO;
+		for(String name:names){
+			int product = nameValue(name) * (1+names.indexOf(name));
+			sum = sum.add(valueOf(product));
+		}
+		return sum.intValue();
+	}
+
+	private int nameValue(String name) {
+		int product = 0;
+		for(Character character:name.toLowerCase().toCharArray()){
+			product += alpha.indexOf(character)+1;
+		}
+		return product;
+	}
+
+	private List<Character> characterArray(String string) {
+		List<Character> characters = new ArrayList<Character>();
+		for(Character character:string.toCharArray()){
+			characters.add(character);
+		}
+		return characters;
+	}
+	
+	private List<String> loadNames(File file) throws FileNotFoundException {
+		List<String> names = new ArrayList<String>();
+		Scanner scanner = new Scanner(file);
+		scanner.useDelimiter(",");
+		while(scanner.hasNext()){
+			names.add(scanner.next().replaceAll("\"", "").trim());
+		}
+		sort(names);
+		return names;
+	}
+	
+}
---
+++ src/main/problem22/NameMagicTest.java
@@ -0,0 +1,76 @@
+package problem22;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+
+public class NameMagicTest {
+	private static final String DICK = "Dick";
+	@Rule
+	public TemporaryFolder folder = new TemporaryFolder();
+	private static final String TOM = "\"Tom\"";
+	private static final String FAKE_FILE_NAME = "fake.txt";
+	private File realFile;
+	private File fakeFile;
+	
+	@Before
+	public void setUp() throws IOException{
+		folder.newFile(FAKE_FILE_NAME);
+		fakeFile = new File(FAKE_FILE_NAME);
+		realFile = new File("src/resources/names.txt");
+	}
+	
+	@Test
+	public void canLoadFileIntoNameMagic() throws Exception {
+		List<String> names = asList(TOM);
+		writeToFile(fakeFile, names);
+		NameMagic magic = new NameMagic(fakeFile);
+		assertThat(magic.sum(), is(20+15+13));
+	}
+	
+	@Test
+	public void canLoadFileWithColinIntoNameMagic() throws Exception {
+		List<String> names = asList("COLIN");
+		writeToFile(fakeFile, names);
+		NameMagic magic = new NameMagic(fakeFile);
+		assertThat(magic.sum(), is(53));
+	}
+
+	@Test
+	public void canAnotherLoadFileIntoNameMagic() throws Exception {
+		List<String> names = asList(TOM,DICK);
+		writeToFile(fakeFile, names);
+		assertThat(new NameMagic(fakeFile).sum(), is(27 + 96));
+	}
+
+
+	@Test
+	public void canRealFileIntoNameMagic() throws Exception {
+		assertThat(new NameMagic(realFile).sum(), is(871198282));
+	}
+
+	private void writeToFile(File fakeFile2, List<String> names) throws Exception {
+		BufferedWriter writer = new BufferedWriter(new FileWriter(fakeFile));
+		for(String name:names){
+			writer.write(name);
+			if(name != names.get(names.size()-1)){
+				writer.write(",");
+			}
+		}
+		writer.flush();
+		writer.close();
+	}
+	
+}
